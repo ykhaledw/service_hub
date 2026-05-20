f74b6309463ca31b3632db318c8a414ae161f1a2
youssefkhaled <youssefwahdan12@gmail.com>
2026-05-20 14:15:43 +0300

    - Created the rest of Login UI - Created a widget for other login tools

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:service_hub/core/helpers/app_spacing.dart';
 import 'package:service_hub/core/helpers/screen_utils.dart';
+import 'package:service_hub/core/shared_widgets/app_button.dart';
+import 'package:service_hub/core/shared_widgets/app_divider.dart';
+import 'package:service_hub/core/shared_widgets/app_text_field.dart';
+import 'package:service_hub/core/shared_widgets/card_container.dart';
 import 'package:service_hub/core/theming/app_colors.dart';
 import 'package:service_hub/core/theming/app_text_styles.dart';
+import 'package:service_hub/features/login/ui/widgets/third_part_auth.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -42,6 +48,74 @@ class LoginScreen extends StatelessWidget {
                   fontSize: AppScreen.sp(16),
                   color: AppColors.black,
                 ),
+              ),
+              AppSpacing.verticalSpace(32),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: AppScreen.w(20)),
+                child: CardContainer(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: AppScreen.w(16),
+                    vertical: AppScreen.h(32),
+                  ),
+                  child: Column(
+                    children: [
+                      AppTextField(label: 'Email'),
+                      AppSpacing.verticalSpace(24),
+                      AppTextField(
+                        label: 'Password',
+                        rightLabel: Text(
+                          'Forgot Password?',
+                          style: AppTextStyles.font14WhiteSemiBold.copyWith(
+                            color: AppColors.primary,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      AppSpacing.verticalSpace(24),
+                      MainButton(onTap: () {}, text: 'Login'),
+                      AppSpacing.verticalSpace(24),
+                      AppDivider(text: 'Or continue with'),
+                      AppSpacing.verticalSpace(24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ThirdPartAuthContainer(
+                            authIcon: Icon(
+                              Icons.person,
+                              size: AppSpacing.iconSizeMd,
+                            ),
+                            authText: 'Google',
+                          ),
+                          AppSpacing.horizontalSpace(16),
+                          ThirdPartAuthContainer(
+                            authIcon: Icon(
+                              Icons.person,
+                              size: AppSpacing.iconSizeMd,
+                            ),
+                            authText: 'Apple',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              AppSpacing.verticalSpace(32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: AppTextStyles.body.copyWith(fontSize: 16.sp),
+                  ),
+                  AppSpacing.horizontalSpace(4),
+                  Text(
+                    'Create Account',
+                    style: AppTextStyles.font14WhiteSemiBold.copyWith(
+                      color: AppColors.primary,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
---
+++ lib/features/login/ui/widgets/third_part_auth.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:service_hub/core/helpers/app_spacing.dart';
+import 'package:service_hub/core/helpers/screen_utils.dart';
+import 'package:service_hub/core/theming/app_colors.dart';
+import 'package:service_hub/core/theming/app_text_styles.dart';
+
+class ThirdPartAuthContainer extends StatelessWidget {
+  final Widget authIcon;
+  final String authText;
+  const ThirdPartAuthContainer({
+    super.key,
+    required this.authIcon,
+    required this.authText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: AppSpacing.borderRadiusLg,
+          border: Border.all(color: AppColors.textHint),
+        ),
+        padding: EdgeInsets.symmetric(vertical: AppScreen.h(10)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            authIcon,
+            AppSpacing.horizontalSpace(8),
+            Text(
+              authText,
+              style: AppTextStyles.font14WhiteSemiBold.copyWith(
+                color: AppColors.textPrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
